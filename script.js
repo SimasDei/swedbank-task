ca5334294f6ac4c749e61df6b3e4820691b0c940
SimasDei <simondeiman@gmail.com>
2019-03-01 10:04:09 +0200

    basic validation and next question rendering

--- script.js
+++ script.js
@@ -1,20 +1,60 @@
 var state = [];
+const elements = {
+  arrows: document.querySelectorAll('.fa-arrow-alt-circle-right'),
+  error: document.querySelector('.error-message'),
+  form: document.querySelector('form')
+};
+
 function questionHandler() {
-  const arrows = document.querySelectorAll('.fa-arrow-alt-circle-right');
+  const arrows = elements.arrows;
 
   arrows.forEach(element => {
     element.addEventListener('click', () => {
       const parent = element.parentElement.parentElement;
       const input = element.previousElementSibling;
-      const nextForm = parent.nextElementSibling;
-      console.log(nextForm);
-      // state.push(input.value);
+      const nextQuestion = parent.nextElementSibling;
+
+      if (
+        input.type === 'text' &&
+        validateForm(input) &&
+        nextQuestion !== null
+      ) {
+        state.push(input.value);
+        nextForm(parent, nextQuestion);
+      } else if (nextQuestion === null) {
+        state.push(input.value);
+        elements.form.textContent = state.toString();
+      }
     });
   });
 }
 
+function nextForm(parent, nextQuestion) {
+  parent.classList.add('inactive');
+  parent.classList.remove('active');
+  nextQuestion.classList.add('active');
+}
+
 function validateForm(input) {
   if (input.value.length < 3) {
+    error(true, 'not enough characters');
+  } else {
+    error(false);
+    return true;
+  }
+}
+
+function error(error, message) {
+  if (error) {
+    document.body.style.background =
+      'linear-gradient(to right, #ED213A, #93291E)';
+    elements.error.textContent = message;
+    elements.error.style.display = 'block';
+  } else {
+    document.body.style.background =
+      'linear-gradient(to right, #134E5E, #71B280)';
+    elements.error.textContent = '';
+    elements.error.style.display = 'none';
   }
 }
 
